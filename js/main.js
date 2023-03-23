2a4edb323d3a651e6997537ed0d3b45bbfea4bf4
Fvrenn <hegetimothe@gmail.com>
2023-03-23 20:49:57 +0100

    3eme section

---
+++ js/main.js
@@ -0,0 +1,39 @@
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+let swiperPortfolio = new Swiper('.portfolio__container', {
+  cssMode: true,
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  /* mousewheel: true,
+  keyboard: true, */
+});
+
+let tabs = document.querySelectorAll('.tabs__toggle'),
+    contents = document.querySelectorAll('.tabs__content'),
+    swipers = document.querySelectorAll('.portfolio__container');
+
+tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+        contents.forEach((content) => {
+            content.classList.remove('is-active');
+        });
+        tabs.forEach((tab) => {
+            tab.classList.remove('is-active');
+        });
+        contents[index].classList.add('is-active');
+        tabs[index].classList.add('is-active');
+        
+        // Initialize swiper in active tab
+        swipers[index].swiper.update();
+    });
+});
